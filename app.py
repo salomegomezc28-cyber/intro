bc412feb807d26685d409748c66442ca25e61250
salomegomezc28-cyber <salomegomezc28@gmail.com>
2026-02-26 10:18:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,6 @@ st.header("aqui empiezo a desarrollar la app")
 st.write("backend y frotend")
 image = Image.open('imagen.jpg')
 st.image(image, caption='interfaces')
+
+texto = st.text_input('escribe algo','este es el texto')
+st.write('El texto escrito es', texto)
