40550dae086058c742eb976481a8952323381362
salomegomezc28-cyber <salomegomezc28@gmail.com>
2026-02-26 10:40:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,27 @@ st.image(image, caption='interfaces')
 
 texto = st.text_input('escribe algo','este es el texto')
 st.write('El texto escrito es', texto)
+
+
+
+st.subheader("ahora son 2 columnas")
+
+col1, col2 = st.columna(2)
+with col1:
+  st.subheader("Esta es la primera columna")
+  st.write("mas texto")
+  resp = st.checkbox("todavia mas texto")
+  if resp:
+    st.write('correcto')
+
+with col2:
+  st.subheader("esta es la segunda columna")
+  modo = st.radio("modalidad de tu interfaz")
+  if modo == 'visual':
+    st.write('factor visual en tu interfaz')
+  if modo == 'auditivo':
+    st.write('factor auditivo en tu interfaz')
+  if modo == 'tactil':
+    st.write('factor tactil en tu interfaz')
+  
+      
